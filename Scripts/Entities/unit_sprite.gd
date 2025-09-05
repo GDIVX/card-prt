b9c26f302fd102259e74bf445eea7fd6fff9e508
GDIVX <yarin107@gmail.com>
2025-09-05 17:01:03 +0300

    hit vfx

--- Scripts/Entities/unit_sprite.gd
+++ Scripts/Entities/unit_sprite.gd
@@ -3,7 +3,7 @@ class_name UnitSprite extends AnimatedSprite2D
 @export var animation_player : AnimationPlayer
 
 func _on_applied_damage() ->void:
-    animation_player.play("flash")
+    animation_player.play("HIT")
 
 
 func _on_health_received_damage(_to_health:int, _to_defense:int) -> void:
