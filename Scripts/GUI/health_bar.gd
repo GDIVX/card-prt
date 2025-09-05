8145eef60df8d0b7c6a5f3d515d03fb423d589b9
GDIVX <yarin107@gmail.com>
2025-09-05 13:30:28 +0300

    health bar widget: show health bar if defense is not 0

--- Scripts/GUI/health_bar.gd
+++ Scripts/GUI/health_bar.gd
@@ -17,35 +17,37 @@ extends TextureProgressBar
 
 func _on_health_health_changed(current_value:int) -> void:
 	# TODO: blink shader would be nice
+	
+	_show_health_widget(current_value)
 	var max_health := health_object.max_health
+	if current_value == max_health and health_object.defense == 0: 
+		_hide_health_widget()
+
+
+
+func _show_health_widget(current_value: int) -> void:
+	var max_health := health_object.max_health
+	var _ratio: float = current_value as float / max_health as float
 
 	if not _health_label:
 		_health_label = $Label
 	_health_label.text = "%s/%s" % [current_value , max_health]
-
-	var _ratio: float = current_value as float / max_health as float
-
 	var tween: Tween = get_tree().create_tween()
 
 	tween.tween_property(self , "value" , _ratio , fill_tween_duration)\
 	.set_trans(fill_tween_trans).set_ease(fill_tween_ease)
 
-	var fill_color = lerp(fill_color_healthy , fill_color_unhealthy , _ratio)
+	var fill_color = lerp(fill_color_unhealthy , fill_color_healthy , _ratio)
 	tween.parallel().tween_property(self, "tint_progress" , fill_color , fill_tween_duration)\
 	.set_trans(fill_tween_trans).set_ease(fill_tween_ease)
 
 
 	tween.parallel().tween_property(_health_label , "self_modulate" , Color.WHITE, fill_tween_duration)
 
-	# if health is full, we can hide the health bar
-	# Play it after the previous animation is done
-	if current_value != max_health: return
+func _hide_health_widget() -> void:
+	var tween: Tween = get_tree().create_tween()
 	tween.tween_property(_health_label , "self_modulate" , Color.TRANSPARENT, fill_tween_duration)
 	tween.parallel().tween_property(self , "self_modulate" , Color.TRANSPARENT, fill_tween_duration)
-
-
-	
-
 
 
 func _on_health_defense_changed(current_value:int) -> void:
@@ -59,3 +61,6 @@ func _on_health_defense_changed(current_value:int) -> void:
 	var color := Color.TRANSPARENT if current_value == 0 else Color.WHITE
 	var tween: Tween = get_tree().create_tween()
 	tween.tween_property(_defense_counter , "self_modulate", color , fill_tween_duration)
+	# if defense is above 0, show health too
+	if current_value == 0 : return
+	_show_health_widget(health_object.current_health)
