44ba3502e1c54a1dad5fd222a98e92da4cfcdbad
GDIVX <yarin107@gmail.com>
2025-09-16 11:41:06 +0300

    Add vulnerability toggle to HurtBox; prevent damage during invulnerability period

--- Scripts/Entities/hurt_box.gd
+++ Scripts/Entities/hurt_box.gd
@@ -1,11 +1,17 @@
 class_name HurtBox extends Area2D
 
 @export var health : Health
+@export var vulnerable : bool = true
 
 func _ready():
     connect("area_entered" , _on_area_entered)
 
 
 func _on_area_entered(hit_box: HitBox) -> void:
+    if not vulnerable : return
     if not hit_box : return
     hit_box.damage.apply_damage(health)
+    # set vulnerable to false to one frame 
+    vulnerable = false
+    await get_tree().create_timer(0.1).timeout
+    vulnerable = true
